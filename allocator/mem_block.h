8e6710cdf4faec42af794e2ad509f2800f5c70f7
Islam <i.kardanov@wisetech.pro>
2021-09-28 11:32:00 +0300

    Добавл методы для получения указателей на данные и на объект блок памяти

--- allocator/mem_block.h
+++ allocator/mem_block.h
@@ -13,31 +13,150 @@ constexpr bool freeBlock = true;
 constexpr bool unfreeBlock = false;
 //! Объявление класса буфера
 
+//! Объявление типа класса блок памяти
+class MemBlock;
+
+/**
+ * @brief Шаблонный метод для получения указателя на данные из объекта блок памяти
+ * @tparam T Тип указателя на данные
+ * @param block Указатель на указатель на объект типа блок памяти
+ * @return Возвращает указатель на данные
+ */
+template<class T>
+T *getData(MemBlock **block);
+
+/**
+ * @brief Шаблонная функция для получения указателя на объект типа блок памяти
+ * @tparam T Тип указателя на указатель данных объекта
+ * @param ptr Указатель на указатель объекта данных
+ * @return Возвращает указатель на объект типа блок памяти
+ */
+template<class T>
+MemBlock *getMemBlock(T **ptr);
+
+/**
+ * @brief Класс определяющий сущность блок(кусочек) памяти в буфере
+ * @details Каждый кусочек памяти состоит из 2-х частей,
+ * хедера с информацией о куске памяти и самой памяти под данные
+ *
+ *      struct MemBlock
+ *
+ *      |-----------|
+ *      | sizeBlock |
+ *      |-----------|
+ *      |statusBlock|
+ *      |-----------|
+ *      | *buffer   |
+ *      |-----------|
+ *      |*prevBlock |
+ *      |-----------|
+ *      |*nextBlock |
+ *      |-----------|
+ *      |   DATA    |
+ *      |-----------|
+ *      |           |
+ *      |    .      |
+ *      |    .      |
+ *      |    .      |
+ *      |-----------|
+ *
+ */
 class MemBlock {
 public:
+    /**
+     * @brief
+     * @param sizeBlock
+     * @param buffer
+     * @param prevBlock
+     * @param nextBlock
+     */
     explicit MemBlock(size_t sizeBlock, MemBuffer *buffer, MemBlock *prevBlock, MemBlock *nextBlock);
+
+    /**
+     * @brief
+     */
     ~MemBlock() = default;
 
+    /**
+     * @brief
+     * @param size
+     */
     void setSizeBlock(size_t size);
+
+    /**
+     * @brief
+     * @param status
+     */
     void setStatusBlock(bool status);
+
+    /**
+     * @brief
+     * @param memBlock
+     */
     void setPrevMemBlock(MemBlock *memBlock);
+
+    /**
+     * @brief
+     * @param memBlock
+     */
     void setNextMemBlock(MemBlock *memBlock);
 
+    /**
+     * @brief
+     * @return
+     */
     size_t getSizeBlock() const;
+
+    /**
+     * @brief
+     * @return
+     */
     bool getStatusBlock() const;
+
+    /**
+     * @brief
+     * @return
+     */
     MemBlock *getPrevMemBlock() const;
+
+    /**
+     * @brief
+     * @return
+     */
     MemBlock *getNextMemBlock() const;
 
+    /**
+     * @brief
+     * @return
+     */
     bool isLastMemBlock() const;
+
+    /**
+     * @brief
+     * @return
+     */
     bool isFirstMemBlock() const;
 
 private:
     size_t sizeBlock_;
     bool statusBlock_;
-    MemBuffer* buffer_;
-    MemBlock* prevBlock_;
-    MemBlock* nextBlock_;
+    MemBuffer *buffer_;
+    MemBlock *prevBlock_;
+    MemBlock *nextBlock_;
 };
+
+
+
+template<class T>
+T *getData(MemBlock **block) {
+    return (T*)((char*)(*block) + sizeof (MemBlock));
+}
+
+
+template<class T>
+MemBlock *getMemBlock(T **ptr) {
+    return (MemBlock*)((char*)(*ptr) - sizeof (MemBlock));
+}
 
 MemBuffer &getMemBufferInstance();
 
--- main.cpp
+++ main.cpp
@@ -3,43 +3,15 @@
 #include "allocator/smart_ptr_type.h"
 #include "test/mem_buffer_test.h"
 
-template<class T>
-T* getData(mem::MemBlock *block) {
-    return (T*)((char*)block + sizeof (mem::MemBlock));
-}
 
-template<class T>
-mem::MemBlock *getBlock(T ptr) {
-    return (mem::MemBlock*)(ptr - sizeof(mem::MemBlock));
-}
+
 
 int main() {
     using namespace mem;
     typedef int Type;
     typedef SmartPtr<Type, SafeSmartPtr<Type>, Allocator<Type>> PtrInt;
 
-    MemBuffer buffer(100);
-    buffer.allocateBuffer();
 
-    MemBlock *m1 =  buffer.allocMemBlock(sizeof (int));
-    MemBlock *m2 = buffer.allocMemBlock(sizeof (int));
-
-    printBlock(m1);
-    printBlock(m2);
-
-    int *i1 = getData<int>(m1);
-    int *i2 = getData<int>(m2);
-
-    *i1 = 12;
-    *i2 = 23;
-
-    std::cout << *i1 << "\t" << *i2 << "\n";
-
-    MemBlock *cm1 = getBlock(i1);
-    MemBlock *cm2 = getBlock(i2);
-
-    printBlock(cm1);
-    printBlock(cm2);
 
     return 0;
 }
